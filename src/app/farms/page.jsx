f08bcc498379f68c817721bd80d0cff26db097f6
Andres Gaviria <cgaviria930@gmail.com>
2025-05-05 10:46:59 -0500

    inicio de rutas y orden

--- src/app/farms/page.jsx
+++ src/app/farms/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
     return (
         <div>
-            register page
+            farms page
         </div>
     )
 }
--- src/app/guies/page.jsx
+++ src/app/guies/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
     return (
         <div>
-            register page
+            guies page
         </div>
     )
 }
