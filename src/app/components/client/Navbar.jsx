e0e0e1e3f067d1fb522936f1a635881023c6cb6b
Andres Gaviria <cgaviria930@gmail.com>
2025-05-05 10:46:34 -0500

    cambio rutas finalizado

--- src/app/components/client/Navbar.jsx
+++ src/app/components/client/Navbar.jsx
@@ -10,13 +10,13 @@ export default function Navbar() {
                 className="shadowwhite fixed top-6 left-1/2 -translate-x-1/2 hidden sm:flex w-3/4 md:w-2/4 h-auhref justify-between items-center
                            font-semibold text-xl md:text-lg lg:text-2xl py-4 px-8 md:px-12 rounded-xl backnavbar shadow-2xl
                            text-white tracking-widest">
-                <Link href="../../pages/teams" className="hover:scale-110 hover:transition-transform">EQUIPOS</Link>
-                <Link href="../../page.jsx" className="hover:scale-110 hover:transition-transform">INICIO</Link>
-                <Link href="../../pages/guies" className="hover:scale-110 hover:transition-transform">GUIAS</Link>
-                <Link href="../../pages/farms" className="hover:scale-110 hover:transition-transform">FARMEO</Link>
+                <Link href="../../teams" className="hover:scale-110 hover:transition-transform">EQUIPOS</Link>
+                <Link href="../../" className="hover:scale-110 hover:transition-transform">INICIO</Link>
+                <Link href="../../guies" className="hover:scale-110 hover:transition-transform">GUIAS</Link>
+                <Link href="../../farms" className="hover:scale-110 hover:transition-transform">FARMEO</Link>
             </div>
 
-            <Link className="" href="/login">
+            <Link className="" href="../../">
                 <svg className="invert size-14 fixed top-6 right-6 cursor-pointer hover:scale-110 transition-transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
