9d6ace65a8886974414f950cf01a74ba12019177
Andres Gaviria <cgaviria930@gmail.com>
2025-05-06 11:50:45 -0500

    Añadido imagenes slider

--- src/app/components/client/Slider.jsx
+++ src/app/components/client/Slider.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
 
 export default function Slider() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch("../../../Data.json") // Reemplaza con la URL correcta de tu JSON
+            .then(response => response.json())
+            .then(data => setCharacters(data))
+            .catch(error => console.error("Error al obtener los datos:", error));
+    }, []);
+
     return (
-        <div>
-        
+        <div className="mt-36 overflow-hidden">
+            <div className="slider md:w-[60vw] w-[85vw]">
+                <div className="slide-track flex">
+                    {characters.concat(characters).map((char, index) => (
+                        <div key={index} className="slide bg-[#222] rounded-xl flex flex-col items-center p-2">
+                            <img src={char.imagen} alt={char.Nombre} className="w-32 h-32 object-contain"/>
+                            <p className="text-white mt-2 text-center font-semibold">{char.Nombre}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
